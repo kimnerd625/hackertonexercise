ef610d7d9045aaf2b6a76abc1412a0df13652507
kimnerd625 <104896647+kimnerd625@users.noreply.github.com>
2023-05-09 17:37:12 +0900

    feat: react-router-dom 설치 및 라우터 설정
    feat: react-router-dom 설치 및 라우터 설정

---
+++ src/components/Main.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+import { PageWrapper, MainPageTitle } from "./Main.styles"
+
+function Main() {
+
+  return (
+    <>
+      <PageWrapper>
+        <MainPageTitle>
+          메인 페이지
+        </MainPageTitle>
+      </PageWrapper>
+    </>
+  )
+}
+
+export default Main
