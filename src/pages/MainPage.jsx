0d3e1d326b72e8ab3c2aa440ff22fac962371293
RJihyeon <03x2_18@yonsei.ac.kr>
2023-05-11 11:10:39 +0900

    feat: title component 만듦

--- src/pages/MainPage.jsx
+++ src/pages/MainPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 
 import Main from "../components/Main/Main";
+import Title from '../components/Title/Title';
 
 function MainPage() {
 
   return (
     <>
+    <Title text = '메인'/>
       <Main />
     </>
   );
